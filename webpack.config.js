a89e3e1be65b94a7adf8ee636c21047c271071aa
Abhishek Acharya <abhishekacharya171@gmail.com>
2022-05-20 16:43:47 +0545

    Added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode : "development",
-	entry : {
-		main: path.resolve(__dirname, 'src/index.js'),
+	mode: "development",
+
+	entry: {
+		main: path.resolve(__dirname, "src/index.js"),
 	},
-	output : {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js', 
-	}, 
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: path.resolve(__dirname, 'dist'),
+		port: 5001,
+		hot: true,
+		open: true,
+	},
+	module: {
+			rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+				// Creates `style` nodes from JS strings
+				"style-loader",
+				// Translates CSS into CommonJS
+				"css-loader",
+				// Compiles Sass to CSS
+				"sass-loader",
+				],
+			},
+		],
+	},
 };
